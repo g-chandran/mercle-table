c356ff965fdf24da60cc15edd6bf3ecb1830823a
Hemachandran G <chandranchan321@gmail.com>
2022-10-16 19:03:37 +0530

    Added search functionality & minor column resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import { SearchBox } from "./components/SearchBox/SearchBox";
 import { Table } from "./components/Table/Table";
 import { MOCKDATA } from "./lib/mockData";
 
 function App() {
+  const [mockData, setMockData] = useState(MOCKDATA.slice(0, 10));
+
+  const searchInputHandler = (value: string) => {
+    const searchResult = MOCKDATA.slice(0, 10).filter((element) =>
+      element.id.includes(value)
+    );
+    setMockData(searchResult);
+  };
+
   return (
     <div className="App">
       <h1>Mercle Table</h1>
-      <SearchBox />
-      <Table data={MOCKDATA} />
+      <SearchBox onChangeHandler={searchInputHandler} />
+      <Table data={mockData} />
     </div>
   );
 }
--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,13 +1,23 @@
 import styles from "./SearchBox.module.css";
 import { FiSearch } from "react-icons/fi";
+import { ChangeEvent } from "react";
 
-export function SearchBox() {
+export function SearchBox({
+  onChangeHandler,
+}: {
+  onChangeHandler: (value: string) => void;
+}) {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChangeHandler(e.currentTarget.value);
+  };
+
   return (
     <div className={styles.search}>
       <div>
         <FiSearch />
       </div>
       <input
+        onChange={onChange}
         type="text"
         name="search-box"
         placeholder="Search"
